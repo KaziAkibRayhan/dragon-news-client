0035d67899aa38a033acef6e415266cdd8abd426
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-12-02 11:00:14 +0600

    project layout design

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCUYD0afYHha5oL4RAaZQKcCuoHMLB1rYA",
+    authDomain: "dragon-news-2119a.firebaseapp.com",
+    projectId: "dragon-news-2119a",
+    storageBucket: "dragon-news-2119a.appspot.com",
+    messagingSenderId: "86658165917",
+    appId: "1:86658165917:web:234270872024ae33d9f874"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layout/Main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import RightSideNav from '../Pages/Shared/RightSideNav/RightSideNav';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
+import Header from '../Pages/Shared/Header/Header';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg='2'>
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg='7'>
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg='3'>
+                        <RightSideNav></RightSideNav>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
